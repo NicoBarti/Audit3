634c8757030a648ec8a08d1dc5f88ab115e55f8e
NicoBarti <nicolas.barticevic@gmail.com>
2017-11-22 19:33:00 -0300

    configura /api en routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :usuarios
+  scope '/api' do
+    resources :usuarios
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
