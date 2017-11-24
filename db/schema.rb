5d763f73579d671e7e42192d3d652c6acc703021
NicoBarti <nicolas.barticevic@gmail.com>
2017-11-24 20:45:23 -0300

    elimina Usuario ya que tengo user de authenticToken

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171123224840) do
+ActiveRecord::Schema.define(version: 20171124234200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,13 +42,6 @@ ActiveRecord::Schema.define(version: 20171123224840) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
-  end
-
-  create_table "usuarios", force: :cascade do |t|
-    t.string "nombre", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["nombre"], name: "index_usuarios_on_nombre", unique: true
   end
 
 end
